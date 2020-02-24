96a176c558c977b34ac41ce6ca81722c8851674e
pentas1150 <beewol@soongsil.ac.kr>
2020-02-24 17:38:39 +0900

    1)input 한글만 입력되는 문제 해결

--- src/components/Input.js
+++ src/components/Input.js
@@ -20,17 +20,14 @@ function Input() {
     setInput(e.target.value);
   };
   const onKeyInput = e => {
-    e.preventDefault();
-
-    if (e.keyCode === 13) {
-      dispatch(addTodo(input));
-      setInput("");
+    if (e.key === "Enter") {
+      addTodoItem(e);
     }
   };
 
   return (
     <InputGroup className="mb-3">
-      <FormControl placeholder="Enter the TODO!!" onChange={onChangeInput} onKeyDown={onKeyInput} value={input} />
+      <FormControl placeholder="Enter the TODO!!" onChange={onChangeInput} onKeyPress={onKeyInput} value={input} />
       <InputGroup.Append>
         <Button variant="link" onClick={addTodoItem}>
           <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
